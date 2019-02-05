336dfd9dc157cf4bb4aa07cddb5f9e326f590e33
Toader <tinika.toader@yandex.ru>
2019-02-05 22:00:58 +0300

    Firts commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+const router = express.Router();
+
+app.use(bodyParser.json());
+
+router.post("/api/test_web_hooks", function(req, res) {
+  console.log(req.body);
+  res.sendStatus(200);
+});
+
+router.get("/api/:id", function(req, res) {
+  console.log(req.params);
+  res.sendStatus(200);
+});
+app.use(router);
+app.listen(3000, () => console.log("http://localhost:3000"));
