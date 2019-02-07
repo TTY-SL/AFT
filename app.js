583596d8cb9f1d7192c1c0cf02b1da0d5c8e9877
Toader <tinika.toader@yandex.ru>
2019-02-07 20:54:12 +0300

    add: cors policy

--- app.js
+++ app.js
@@ -6,6 +6,14 @@ const router = express.Router();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 router.post("/api/test_web_hooks", function(req, res) {
   console.log("Post request => ", req.body);
