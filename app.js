6941a196981e9ac6653c9f6813909cf5305eac68
TTY-SL <tinika.toader@gmail.com>
2019-07-19 14:30:15 +0300

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const bodyParser = require("body-parser");
 const app = express();
 const router = express.Router();
 
-app.use(bodyParser.json());
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 // app.use(bodyParser.urlencoded({ extended: true }));
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
