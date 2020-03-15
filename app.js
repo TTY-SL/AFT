80427714d51b6043374b8a99f34c40f76998845b
TTY-SL <tinika.toader@gmail.com>
2020-03-15 11:44:31 +0300

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   next();
 });
 app.use(router);
