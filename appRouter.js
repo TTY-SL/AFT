4266645e893eb8469c08feea5fee08262f6dc448
Toader <tinika.toader@yandex.ru>
2020-09-01 19:32:11 +0300

    Add (route):  AS uri for scef.

--- appRouter.js
+++ appRouter.js
@@ -1,31 +1,41 @@
 const express = require("express");
 
-const { broadcast } = require("./websocket");
+const {
+  broadcast
+} = require("./websocket");
 
 const router = express.Router();
 
-router.post("/api/test_web_hooks", function(req, res) {
+router.post("/api/test_web_hooks", function (req, res) {
   console.log("Post request => ", req.body);
   broadcast("/api/test_web_hooks", req.body);
   res.sendStatus(200);
 });
 
-router.post("/simevents", function(req, res) {
+router.use("/api/scef_as", function (req, res) {
+  console.log("Post request => ", req.body);
+  broadcast("/api/scef_as" + req.url, req.body);
+  res.sendStatus(200);
+});
+
+router.post("/simevents", function (req, res) {
   console.log("Post request => ", req.body);
   broadcast("/simevents", req.body);
   res.sendStatus(200);
 });
 
-router.get("/api/:id", function(req, res) {
+router.get("/api/:id", function (req, res) {
   console.log("Test connection=> ", req.params);
   broadcast("/api/:id", req.params);
   res.sendStatus(200);
 });
-router.get("/", function(req, res) {
+router.get("/", function (req, res) {
   res.status(200).send("Hellow");
 });
-router.get("/websocket", function(req, res) {
-  res.sendFile("/index.html", { root: __dirname });
+router.get("/websocket", function (req, res) {
+  res.sendFile("/index.html", {
+    root: __dirname
+  });
 });
 
 module.exports = router;
